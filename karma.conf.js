175dedfc1d64b18e8acbb4ed9ba8ef4d92b085be
Benoît Sauveton <bsauveton@gmail.com>
2022-08-10 21:13:10 +0200

    update name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-front'),
+      dir: require('path').join(__dirname, './coverage/demo-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-front'`, () => {
+  it(`should have as title 'demo-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-front');
+    expect(app.title).toEqual('demo-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demo-frontend app is running!');
   });
 });
